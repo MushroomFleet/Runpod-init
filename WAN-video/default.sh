979f196ebbca137c52b5c4d0cfd6976f7f619f6c
Tom <mushroomfleet@gmail.com>
2025-02-28 21:03:12 +0000

    Update default.sh

--- WAN-video/default.sh
+++ WAN-video/default.sh
@@ -70,7 +70,7 @@ DIFFUSION_MODELS=(
     #"https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-T2V-1_3B_bf16.safetensors"
     "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-T2V-14B_fp8_e4m3fn.safetensors"
     "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-I2V-14B-480P_fp8_e4m3fn.safetensors"
-    #"https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-I2V-14B-720P_fp8_e4m3fn.safetensors"
+    "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-I2V-14B-720P_fp8_e4m3fn.safetensors"
 )
 
 CLIP_MODELS=(
